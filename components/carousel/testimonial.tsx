ba8376731979b09b493b8d60f310518583b4566e
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-01-18 12:22:02 +0100

    feat: update

--- components/carousel/testimonial.tsx
+++ components/carousel/testimonial.tsx
@@ -29,7 +29,7 @@ const TestimonialSection = () => {
   ];
 
   return (
-    <div className="relative w-full mx-auto md:w-1/2 ">
+    <div className="relative w-full mx-auto md:w-1/2 overflow-x-hidden ">
       <Swiper
         modules={[Pagination, Autoplay, Navigation]}
         spaceBetween={30}
@@ -69,7 +69,7 @@ const TestimonialSection = () => {
       <button className="custom-prev absolute left-0 bottom-0 z-20 p-2 hover:opacity-75 transition-opacity">
         <ArrowLeft />
       </button>
-      <div className="custom-pagination absolute bottom-0  left-[50%] -translate-y-1/2 z-20" />
+      <div className="custom-pagination   -translate-y-1/2  bottom-0 left-1/2 m-auto  absolute w-full   z-20" />
       <button className="custom-next absolute right-0 bottom-0 z-20 p-2 hover:opacity-75 transition-opacity">
         <ArrowRight />
       </button>
